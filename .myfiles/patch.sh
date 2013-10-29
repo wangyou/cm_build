fba34f31451854f5b0493cf39d9ae6760c881a8c
nx111 <gdzdz@163.com>
2013-10-29 13:47:03 +0800

    fix patch script for jbx-kernel

--- .myfiles/patch.sh
+++ .myfiles/patch.sh
@@ -35,8 +35,8 @@ fi
 
 ### jbx-kernel patch ###########
 if [ -f $basedir/kernel/motorola/omap4-common-jbx/arch/arm/mach-omap2/opp4xxx_data.c ]; then
-	grep "^\s*OPP_INITIALIZER(\"gpu\", \"dpll_per_m7x2_ck\", \"core\", true, 100300000, OMAP4430_VDD_CORE_OPP25_UV)," arch/arm/mach-omap2/opp4xxx_data.c >/dev/null
-	if [ $? -eq 1 ]; then
+	grep "^\s*OPP_INITIALIZER(\"gpu\", \"dpll_per_m7x2_ck\", \"core\", true, 153600000, OMAP4430_VDD_CORE_OPP50_UV)," $basedir/kernel/motorola/omap4-common-jbx/arch/arm/mach-omap2/opp4xxx_data.c >/dev/null
+	if [ $? -eq 0 ]; then
 		cd $basedir/kernel/motorola/omap4-common-jbx
 		patch -N -p1 <$rdir/jbx-kernel.diff
 		cd $rdir
