8f59d45fd4810617d65d32bbffea4ffd61969c92
nx111 <gdzdz@163.com>
2013-10-29 22:28:49 +0800

    remove CMAccount

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+device=edison
 rdir=`dirname $0`
 [ "$rdir" != "." ] && cd $rdir
 if [ ! -f build/envsetup.sh ]; then
@@ -11,16 +12,16 @@ fi
 
 .myfiles/patch.sh
 
-lunch cm_edison-userdebug 
+lunch cm_$device-userdebug 
 if [ "$1" = "jbx" ]; then
-	make bacon -j4 TARGET_BOOTLOADER_BOARD_NAME=edison TARGET_KERNEL_SOURCE=kernel/motorola/omap4-common-jbx  TARGET_KERNEL_CONFIG=mapphone_OCEdison_defconfig  
-	if [ $? -eq 0 -a "$2" = "kernel" ]; then	
-		[ -d out/target/product/edison/jbx-kernel/rls/system/lib/modules ] || mkdir -p out/target/product/edison/jbx-kernel/rls/system/lib/modules/
-		[ -d out/target/product/edison/jbx-kernel/rls/system/etc/kexec ] || mkdir -p out/target/product/edison/jbx-kernel/rls/system/etc/kexec/
-		cp -r out/target/product/edison/system/lib/modules/* out/target/product/edison/jbx-kernel/rls/system/lib/modules/
-		cp out/target/product/edison/kernel out/target/product/edison/jbx-kernel/rls/system/etc/kexec/
+	make bacon -j4 TARGET_BOOTLOADER_BOARD_NAME=$device TARGET_KERNEL_SOURCE=kernel/motorola/omap4-common-jbx  TARGET_KERNEL_CONFIG=mapphone_OCEdison_defconfig  
+	if [ $? -eq 0 -a "$2" = "kernel" -a "$device" = "edison" ]; then	
+		[ -d out/target/product/$device/jbx-kernel/rls/system/lib/modules ] || mkdir -p out/target/product/$device/jbx-kernel/rls/system/lib/modules/
+		[ -d out/target/product/$device/jbx-kernel/rls/system/etc/kexec ] || mkdir -p out/target/product/$device/jbx-kernel/rls/system/etc/kexec/
+		cp -r out/target/product/$device/system/lib/modules/* out/target/product/$device/jbx-kernel/rls/system/lib/modules/
+		cp out/target/product/$device/kernel out/target/product/$device/jbx-kernel/rls/system/etc/kexec/
 		curdir=`pwd`
-		cd out/target/product/edison/jbx-kernel/rls/
+		cd out/target/product/$device/jbx-kernel/rls/
 		zip -r "../JBX-Kernel-1.4-Hybrid-Edison-4.3_$(date +"%Y-%m-%d").zip" *
 		cd $curdir
 	fi
@@ -28,8 +29,8 @@ else
 	make -j4 bacon 
 fi
 
-[ -d out/target/product/edison ] || exit
-sf=`find out/target/product/edison -name cm-*-UNOFFICIAL-edison.zip`
+[ -d out/target/product/$device ] || exit
+sf=`find out/target/product/$device -name cm-*-UNOFFICIAL-edison.zip`
 [ "$sf" = "" ] && exit
 
 farray=($sf)
@@ -37,3 +38,6 @@ for f in ${farray[@]}
 do
 	 mv $f `echo $f|sed -e "s/UNOFFICIAL/NX111/"`
 done
+
+rm -f out/target/product/$device/cm_$device-ota-*.zip
+rm -f out/target/product/$device/cm-*.zip.md5sum
