46a648a23d702e56ced5b06ccc7077453673e1e4
nx111 <gd.zhangdz@gmail.com>
2014-01-23 20:49:54 +0800

    update repack_gapps.sh

--- .myfiles/scripts/repack_gapps.sh
+++ .myfiles/scripts/repack_gapps.sh
@@ -17,7 +17,7 @@ fi
 outfile=`basename $1| sed -e "s/pa_gapps/gapps/" -e "s/-modular//" -e "s/-full//" `
 
 edir=`mktemp -d /tmp/gapps_XXXXXX`
-echo "Unpacking $1..."
+echo "Unpacking << $1..."
 unzip $1 -d $edir >/dev/null
 sed -e "/\/system\/app\/Calendar.apk/d" \
     -e "s/PA\ *GApps\ *Modular\ *-\ *Mini/GApps/g" \
@@ -26,6 +26,16 @@ sed -e "/\/system\/app\/Calendar.apk/d" \
 for f in $edir/system/addon.d/*; do
 	sed -e "/rm -f \/system\/app\/Calendar\.apk/d" -i $f
 done
+[ -f $edir/delete-list.txt ] &&
+	sed -e "/\/system\/app\/Calendar\.apk/d" -i $edir/delete-list.txt
+[ -f $edir/gapps-list.txt ] &&
+	sed -e "/\/system\/app\/GoogleCalendar\.apk/d" \
+	    -e "/\/system\/app\/PlayGames\.apk/d" \
+	    -e "/\/system\/app\/Books\.apk/d" \
+	    -e "/\/system\/app\/Magazines\.apk/d" \
+	    -e "/\/system\/app\/CalendarGoogle\.apk/d" \
+	    -i $edir/gapps-list.txt
+
 
 #rm -f $edir/system/app/GoogleHome.apk
 rm -f $edir/system/app/GoogleCalendar.apk
@@ -34,8 +44,13 @@ rm -f $edir/system/app/Books.apk
 rm -f $edir/system/app/Magazines.apk
 rm -f $edir/system/app/CalendarGoogle.apk
 
+if [ -f $edir/system/etc/g.prop ]; then
+	gapps_size=`du -s $edir/system|cut -f1`
+	sed -e "s/\(ro.addon.pa_size=\).*/\1$gapps_size/" -i $edir/system/etc/g.prop
+fi
+
 cd $edir
-echo "Repacking $dst/$outfile..."
+echo "Repacking >> $dst/$outfile..."
 zip $dst/$outfile -r * >/dev/null
 cd $curdir
 rm -rf $edir
