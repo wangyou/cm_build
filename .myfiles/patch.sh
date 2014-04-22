dbf719711ee35fc0b475ba4612ded94b7826e00b
nx111 <gd.zhangdz@gmail.com>
2014-04-22 23:59:34 +0800

    revert "Not use ccache when diffrent kernel or device" and use origin cpu freq

--- .myfiles/patch.sh
+++ .myfiles/patch.sh
@@ -359,6 +359,8 @@ if [ "$device" != "mb526" ]; then
             sed -i $basedir/kernel/motorola/omap4-common/arch/arm/configs/${kernel_config} \
                 -e "s/# CONFIG_MAPPHONE_EDISON is not set/CONFIG_MAPPHONE_EDISON=y/g" \
                 -e "s/CONFIG_CPU_FREQ_DEFAULT_GOV_KTOONSERVATIVE=y/# CONFIG_CPU_FREQ_DEFAULT_GOV_KTOONSERVATIVE is not set/g" \
+                -e "s/^CONFIG_OMAP_SMARTREFLEX_CUSTOM_SENSOR=y/# CONFIG_OMAP_SMARTREFLEX_CUSTOM_SENSOR is not set/g" \
+                -e "s/^CONFIG_OMAP_OCFREQ_12=y/# CONFIG_OMAP_OCFREQ_12 is not set/g" \
                 -e "s/# CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX is not set/CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX=y/g" \
                 -e "s/# CONFIG_NLS_UTF8 is not set/CONFIG_NLS_UTF8=y/g"
          elif [ "$device" = "targa" ]; then
