4ba4646a74f112a49ab2d1f695fed62161a0b80c
nx111 <gd.zhangdz@gmail.com>
2014-12-31 00:29:36 +0800

    fix repack_gapps.sh

--- .myfiles/scripts/repack_gapps.sh
+++ .myfiles/scripts/repack_gapps.sh
@@ -46,7 +46,7 @@ edir=`mktemp -d /tmp/gapps_XXXXXX`
 echo "Unpacking << $1..."
 unzip -q $1 -d $edir >/dev/null 2>/dev/null
 
-if [ `du -b $edir/META-INF/com/google/android/updater-script | cut -f1` -lt 5120 ]; then
+if [ `du -b $edir/META-INF/com/google/android/updater-script | cut -f1` -lt 128 ] && [ -f $edir/installer.data ]; then
    #gapp installer v2.0
    cp -r $mydir/gapps/* $edir/
    rm -rf $edir/META-INF/com/google/android/updater-script
